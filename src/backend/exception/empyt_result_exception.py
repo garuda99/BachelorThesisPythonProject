5af3e3193f63b06c17839f79bdc5faeb0cd14c68
Simon Garuda Linder <e11728330@student.tuwien.ac.at>
2023-02-20 03:56:34 +0100

    Revert "initial Commit"
    This reverts commit 820e7dc25205e4d302e39d250f55ae028a9b8852.

--- src/backend/exception/empyt_result_exception.py
+++
@@ -1,9 +0,0 @@
-# thanks to https://stackoverflow.com/a/1319675 and https://stackoverflow.com/a/10270732 for providing an example of
-# how to create custom exceptions
-
-# The exception that will be thrown if a database request does not return anything
-class EmptyResultException(Exception):
-    def __init__(self, message, error_code):
-        self.message = message
-        self.errorCode = error_code
-        super(EmptyResultException, self).__init__(message)
--- src/backend/exception/too_large_result_exception.py
+++
@@ -1,6 +0,0 @@
-# The exception will be thrown if one DB row was expected but multiple were returned by the request
-class TooLargeResultException(Exception):
-    def __init__(self, message, error_code):
-        self.message = message
-        self.errorCode = error_code
-        super(TooLargeResultException, self).__init__(message)
